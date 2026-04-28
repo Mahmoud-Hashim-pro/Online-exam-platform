859525696802d011787906b89957ad0384978059
super-user418 <20246070@stud.fci-cu.edu.eg>
2026-04-29 02:35:27 +0300

    startup

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-import express from 'express'
+const path = require('path');
+const express = require('express');
+
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send("Hello server!");
+app.set('view engine', 'ejs');
+app.use('/', express.static(path.join(__dirname, "/public")))
+
+app.get('/', (req, res) => {
+	res.render('index.ejs', {
+		pageTitle: "My Awesome Website",
+		logoName: "Quizer"
+	})
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (req, res) => {
 	console.log(`http://localhost:${PORT}`);
-})
+});
