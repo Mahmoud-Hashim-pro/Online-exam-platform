8a04cb5ede140b3678c1687b793e6b3476d8a93d
super-user418 <20246070@stud.fci-cu.edu.eg>
2026-04-30 18:04:22 +0300

    added 404, about, log-in, and register pages

--- index.js
+++ index.js
@@ -3,13 +3,38 @@ const express = require('express');
 
 const app = express();
 
+const sharedRenderData = {
+	pageTitle: "My Awesome Website",
+	logoName: "Quizer"
+}
+
 app.set('view engine', 'ejs');
-app.use('/', express.static(path.join(__dirname, "/public")))
+// app.use('/', express.static(path.join(__dirname, "/public")))
 
 app.get('/', (req, res) => {
-	res.render('sign-in.ejs', {
-		pageTitle: "My Awesome Website",
-		logoName: "Quizer"
+	res.render('sign-in.ejs', sharedRenderData)
+})
+
+app.get('/log', (req, res) => {
+	res.render('log-in.ejs', sharedRenderData)
+})
+
+app.get('/reg', (req, res) => {
+	res.render('register.ejs', sharedRenderData)
+})
+
+app.get('/about', (req, res) => {
+	res.render('about.ejs', sharedRenderData)
+})
+
+// Handle Unknown URLs.
+// Source - https://stackoverflow.com/a/79554232
+// Posted by Donggi Kim, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-04-30, License - CC BY-SA 4.0
+app.all('/{*any}', (req, res, next) => {
+	res.status(404).render('404.ejs', {
+		sharedRenderData,
+		page: req.path
 	})
 })
 
